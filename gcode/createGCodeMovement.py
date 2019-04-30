4b19a7fb0fb36733bf4bb03c4a4d95c6e5b01482
tetov <a@tetov.se>
2019-04-30 13:08:16 +0200

    Refactoring and fixed comp name

--- gcode/createGCodeMovement.py
+++ gcode/createGCodeMovement.py
@@ -8,15 +8,16 @@
 """
 
 __author__ = "tetov"
+__date__ = "20190430"
 
 import Rhino.Geometry as rg
 import Grasshopper.Kernel as gh
 
 w = gh.GH_RuntimeMessageLevel.Warning
 
-ghenv.Component.Name = "Offset inwards completely"
-ghenv.Component.NickName = 'offsetInwardsCompletely'
-
+ghenv.Component.Name = 'createGcodeMovement'
+ghenv.Component.NickName = 'gMovement'
+ghenv.Component.Message = __author__ + " " + __date__
 
 ghenv.Component.Params.Input[1].NickName = "pts"
 ghenv.Component.Params.Input[1].Name = "Points"
@@ -27,27 +28,38 @@ ghenv.Component.Params.Input[2].Name = "Travel speed"
 ghenv.Component.Params.Input[3].NickName = "autohome"
 ghenv.Component.Params.Input[3].Name = "Autohome all axis at start?"
 
-
 ghenv.Component.Params.Output[1].NickName = "gcode"
 ghenv.Component.Params.Output[1].Name = "gcode commands"
 
-gcode = []
-
-def generate_g1(input_pt):
-    g1_command = "G1 F%d X%.3f Y%.3f Z%.3f" % (speed, input_pt.X, input_pt.Y, input_pt.Z)
-    return g1_command
-
-if not speed: speed = 1500
-elif speed[0] <= 0: raise ValueError('Zero or negative speed values not accepted')
+if not speed:
+    speed = 1500
+elif speed[0] <= 0:
+    raise ValueError('Zero or negative speed values not accepted')
 else:
     if len(speed) > 1:
-        ghenv.Component.AddRuntimeMessage(w, 'Only a single speed value is currently supported.')
+        ghenv.Component.AddRuntimeMessage(
+            w, 'Only a single speed value is currently supported.')
 
     speed = int(speed[0])
 
-if autohome: gcode.append("G28")
 
-for pt in pts:
-    pt = rg.Point3d(pt)
-    gcode.append(generate_g1(pt))
+def generate_g1(input_pt):
+    g1_command = "G1 F%d X%.3f Y%.3f Z%.3f" % (speed, input_pt.X, input_pt.Y,
+                                               input_pt.Z)
+    return g1_command
 
+
+def main():
+    commands = []
+
+    if autohome:
+        commands.append("G28")
+
+    for pt in pts:
+        pt = rg.Point3d(pt)
+        commands.append(generate_g1(pt))
+
+    return commands
+
+
+gcode = main()
